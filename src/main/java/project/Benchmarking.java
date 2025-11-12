e6699baafcbb89723393a90823627d3b6c17ec7a
marcokeilwagen <marco.keilwagen@nordakademie.de>
2025-11-12 20:21:20 +0000

    feat: add Benchmarking class

--- src/main/java/project/Benchmarking.java
+++ src/main/java/project/Benchmarking.java
@@ -1,8 +1,38 @@
 package project;
 
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.SECONDS)
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+@Fork(value = 1)
+
 public class Benchmarking {
-    public static void main(String[] args) {
-        Matrix matrix = new Matrix();
-        matrix.compile();
+
+    @Param({"128", "256"})
+    int n;
+
+    private Matrix matrix;
+
+    @Setup(Level.Trial)
+    public void setupTrial() {
+        matrix = new Matrix(new Random(), n);
+    }
+
+    @Setup(Level.Invocation)
+    public void setupInvocation() {
+        matrix.clearC();
+    }
+
+    @Benchmark
+    public void multiply(Blackhole bh) {
+        matrix.multiply();
+        bh.consume(matrix.peek());
     }
 }
